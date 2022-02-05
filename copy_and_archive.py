2db9bf57c093b219f37b336117dfe5bfd4bb986e
sinnan-security <sinnan.suleman@daraz.pk>
2022-02-05 13:58:08 +0500

    Commits include: Lab1 Update disk preservation. Lab1 Important commands related to disk mounting.

--- copy_and_archive.py
+++ copy_and_archive.py
@@ -6,20 +6,15 @@ tm=str(time.time())
 blocksize=512
 bytes=b'x00'
 evidence=open('evidence__'+tm+'.dd','wb')
+hash_algo=hashlib.sha256()
 while bytes:
 	bytes=source.read(blocksize)
 	evidence.write(bytes)
-source.close()
-evidence.close()
-
-compressed=tarfile.open('evidence__'+tm+'.tar','w:xz')
-compressed.add('evidence__'+tm+'.dd')
-compressed.close()
-
-hash_algo=hashlib.sha256()
-evidence=open('evidence__'+tm+'.tar','rb')
-while bytes:
-	bytes=evidence.read(blocksize)
 	hash_algo.update(bytes)
 hash_algo.update(tm.encode())
 print(hash_algo.hexdigest())
+source.close()
+evidence.close()
+compressed=tarfile.open('evidence__'+tm+'.tar','w:xz')
+compressed.add('evidence__'+tm+'.dd')
+compressed.close()
