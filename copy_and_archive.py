4dd04e71d1d37e8d8b0148c520826cb83dcf67ff
sinnan-security <sinnan.suleman@daraz.pk>
2022-02-18 16:37:36 +0500

    Yara signature matching. Thats lecture 2

--- copy_and_archive.py
+++ copy_and_archive.py
@@ -1,18 +1,23 @@
 import time
 import tarfile
 import hashlib
-source=open('/dev/loop5','rb')
+import argparse
+
+parser = argparse.ArgumentParser(description='backup a device into an image')
+parser.add_argument('device', help='the device to use')
+parser.add_argument('-b',type=int,default=512,help='reading blocksize')
+arg=parser.parse_args()
+source=open(arg.device,'rb')
 tm=str(time.time())
-blocksize=512
 bytes=b'x00'
 evidence=open('evidence__'+tm+'.dd','wb')
 hash_algo=hashlib.sha256()
 while bytes:
-	bytes=source.read(blocksize)
+	bytes=source.read(arg.b)
 	evidence.write(bytes)
 	hash_algo.update(bytes)
 hash_algo.update(tm.encode())
-print(hash_algo.hexdigest())
+hash_algo.hexdigest()
 source.close()
 evidence.close()
 compressed=tarfile.open('evidence__'+tm+'.tar','w:xz')
